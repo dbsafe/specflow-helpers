aeb119b8f4b04e6b7e70a49623d69903ccf9b43c
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-12-04 07:41:45 -0500

    Added database project for demo. Added new projects.

---
+++ Demo.Database.Tests/DatabaseTestSteps.cs
@@ -0,0 +1,52 @@
+﻿using DbSafe;
+using Specflow.Steps.Db;
+using SqlDbSafe;
+using TechTalk.SpecFlow;
+
+namespace Demo.Database.Tests
+{
+    [Binding]
+    public class DatabaseTestSteps
+    {
+        private SpecflowDbPopulator _populator;
+        private SpecflowDbValidator _validator;
+
+        private IDbSafeManager _dbSafe;
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            var dbSafe = SqlDbSafeManager.Initialize("specflow-helpers-demo.xml");
+            dbSafe.SetConnectionString(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;Initial Catalog=specflow-helpers-demo");
+            dbSafe.ExecuteScripts("delete-products");
+
+            _dbSafe = dbSafe;
+
+            _populator = new SpecflowDbPopulator();
+            _validator = new SpecflowDbValidator();
+        }
+
+        [AfterScenario]
+        public void Cleanup()
+        {
+            _dbSafe?.Completed();
+        }
+
+        [Given(@"table '(.*)' contains the data")]
+        public void SetTable(string tableName, Table table)
+        {
+            _populator.SetTable(tableName, table);
+        }
+
+        [When(@"I execute my operation")]
+        public void ExecuteOperation()
+        {
+        }
+
+        [Then(@"table '(.*)' should contain the data")]
+        public void AssertTable(string tableName, Table table)
+        {
+            _validator.AssertTable(tableName, table);
+        }
+    }
+}
---
+++ Specflow.Steps.Db/SpecflowDbPopulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Specflow.Steps.Db
+{
+    public class SpecflowDbPopulator
+    {
+        public void SetTable(string tableName, Table table)
+        {
+
+        }
+    }
+
+    public class SpecflowDbValidator
+    {
+        public void AssertTable(string tableName, Table table)
+        {
+
+        }
+    }
+}
