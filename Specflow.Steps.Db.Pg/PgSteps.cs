922390aa52ea8f8aa7cbd1064032bafb12c094a9
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2022-02-26 14:05:10 -0500

    Added PgSteps.

---
+++ Specflow.Steps.Db.Pg/PgSteps.cs
@@ -0,0 +1,26 @@
+﻿using DbSafe;
+using Npgsql;
+using Specflow.Steps.Db.Shared;
+
+namespace Specflow.Steps.Db.Pg
+{
+    public class PgSteps : DbSteps
+    {
+        public PgSteps(string connectionString, FormatterManager formatter = null)
+            : base(
+                  BuildPopulator(connectionString),
+                  BuildValidator(connectionString, formatter))
+        {
+        }
+
+        private static SpecflowDbPopulator<NpgsqlConnection, NpgsqlCommand> BuildPopulator(string connectionString)
+        {
+            return new SpecflowDbPopulator<NpgsqlConnection, NpgsqlCommand>(connectionString, new PgSpecflowDbSchema(connectionString));
+        }
+
+        private static SpecflowDbValidator<NpgsqlConnection, NpgsqlCommand> BuildValidator(string connectionString, FormatterManager formatter)
+        {
+            return new SpecflowDbValidator<NpgsqlConnection, NpgsqlCommand>(new PgSpecflowDbSchema(connectionString), formatter);
+        }
+    }
+}
